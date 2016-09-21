54d75104c9bb24aca4ca6b424243a17336e7dcb2
Alexa Bagnard <abagnard@gmail.com>
2016-09-20 22:27:44 -0700

    ajax call to dictionary works. need to format the data coming back

--- background.js
+++ background.js
@@ -2,7 +2,23 @@ let vocabWord = "";
 let valid;
 
 // Set up context menu at install time.
-// chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    "title": "Add to Vocab-U-List",
+    "contexts": ["all"],
+    'id': 'contextMenuId'
+  });
+});
+
+//redefine global variables
+chrome.runtime.onMessage.addListener(function(request) {
+  vocabWord = request.word;
+  valid = request.valid;
+});
+
+// chrome.runtime.onMessage.addListener(function(request) {
+//   vocabWord = request.word;
+//   valid = request.valid;
 //   chrome.contextMenus.create({
 //     "title": "Add to Vocab-U-List",
 //     "contexts": ["all"],
@@ -10,26 +26,11 @@ let valid;
 //   });
 // });
 
-chrome.runtime.onMessage.addListener(function(request) {
-  // alert("background onMessage being called!");
-  if(request.type === 'createContextMenu') {
-    vocabWord = request.word;
-    valid = request.valid;
-    chrome.contextMenus.create({
-      "title": "Add to Vocab-U-List",
-      "contexts": ["all"],
-      'id': 'contextMenuId'
-    });
-  }
-});
-// chrome.runtime.onMessage.addListener(function(request) {
-//   vocabWord = request.word;
-// });
-
-
+//called when the user clicks on the contextMenus' option
 chrome.contextMenus.onClicked.addListener(function() {
   if (valid === true) {
-    alert("you added " + vocabWord + " to your list!");
+    alert(`you added "${vocabWord}" to your list!`);
+    getDefinition();
   } else if (vocabWord === "") {
     alert("Please select a word to add to your Vocab-U-List");
   } else {
@@ -38,17 +39,6 @@ chrome.contextMenus.onClicked.addListener(function() {
   }
 });
 
-// // Called when the user clicks on the contextMenus' option
-// chrome.contextMenus.onClicked.addListener(function(word, tab) {
-//   alert(word.selectionText);
-//   // Send a message to the active tab
-//   // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-//   //   var activeTab = tabs[0];
-//   // console.log("you did it!");
-//   // alert("you did it!");
-//   chrome.tabs.create({"url": "http://google.com"});
-//   // chrome.tabs.sendMessage(tab.id, {"message": "clicked_browser_action"});
-// });
 
 
 // chrome.runtime.onMessage.addListener(
@@ -59,30 +49,29 @@ chrome.contextMenus.onClicked.addListener(function() {
 //   }
 // );
 
-// function getDefinition(word){
-//   let dictionaryURL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word + "?key=d3f5c888-db47-4c06-8c91-c65cc8a39137";
-//
-//   //initialize ajax call to dictionary.php to get meaning of the word.
-//   let xmlhttpRequest = new XMLHttpRequest();
-//   xmlhttpRequest.open("GET", dictionaryurl, true); //operate translate in async mode.
-//   xmlhttpRequest.send();
-//
-//   xmlhttpRequest.onreadystatechange = function () {
-//     if (xmlhttpRequest.readyState === 4) {
-//       if (xmlhttpRequest.status === 200) {
-//           alert("Picture dictionary lookup returned with response = " + xmlhttpRequest.responseText);
-//           //decode json to get the translated text.
-//       } else {
-//         console.log("error");
-//       }
-//     }
-//   };
-// }
-//
-// function saveWord(){
-//
-// }
-//
+
+
+//called when word successfully added to list
+function getDefinition(){
+  let dictionaryURL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + vocabWord + "?key=d3f5c888-db47-4c06-8c91-c65cc8a39137";
+
+  //initialize ajax call to dictionary.php to get meaning of the word.
+  let xmlhttpRequest = new XMLHttpRequest();
+  xmlhttpRequest.open("GET", dictionaryURL, true); //operate translate in async mode.
+  xmlhttpRequest.send();
+
+  xmlhttpRequest.onreadystatechange = function () {
+    if (xmlhttpRequest.readyState === 4) {
+      if (xmlhttpRequest.status === 200) {
+          alert("Picture dictionary lookup returned with response = " + xmlhttpRequest.responseText);
+          //decode json to get the translated text.
+      } else {
+        alert("**ERROR** Word cannot be found in dictionary. Unable to be added to Vocab-U-List.");
+      }
+    }
+  };
+}
+
 
 //MY DICTIONARY INFO:
 // let dictionaryAPI = "d3f5c888-db47-4c06-8c91-c65cc8a39137";
--- content.js
+++ content.js
@@ -13,50 +13,17 @@
 
 document.addEventListener("mousedown", function(event){
     if (event.button === 2) {
-      // alert("in mousedown event!");
-      // let word = "";
-
       let word = window.getSelection().toString();
       if(word.length > 0 && word.indexOf(" ") === -1) {
-        // alert(word.indexOf(" "));
-        // alert(word.length);
-              //get selected text and send request to bkgd page to create menu
         chrome.runtime.sendMessage({
           'word': word,
-          'type': 'createContextMenu',
           'valid': true
         });
-
       } else {
         chrome.runtime.sendMessage({
           'word': word,
-          'type': 'createContextMenu',
           'valid': false
         });
       }
     }
-
 }, true);
-
-// document.addEventListener('mouseup',function()
-// {
-//     var selectedWord = window.getSelection().toString();
-//     var url = window.location.href;
-//     if(selectedWord.length > 0) {
-//       chrome.runtime.sendMessage(
-//         {
-//           'type': 'selected',
-//           'selection': selectedWord
-//         }
-//       );
-//
-// }});
-
-
-// if(selectedWord.length > 0)
-//     chrome.extension.sendRequest({
-//       'message':'setText',
-//       'data': selectedWord,
-//       'url': url
-//     },
-//     function(response){})
