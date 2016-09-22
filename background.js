f497e7e98102b5816bdfc48008b56fdfae8ab48b
Alexa Bagnard <abagnard@gmail.com>
2016-09-22 15:56:47 -0700

    only 4 defs get stored in storage. if no word type...alerts user that word not in dict

--- background.js
+++ background.js
@@ -32,7 +32,7 @@ chrome.runtime.onMessage.addListener(function(request) {
 //called when the user clicks on the contextMenus' option
 chrome.contextMenus.onClicked.addListener(function() {
   if (valid === true) {
-    getDefinition();
+    dictionaryAjax();
     alert(`you added "${vocabWord}" to your list!`);
   } else if (vocabWord === "") {
     alert("Please select a word to add to your Vocab-U-List");
@@ -47,25 +47,28 @@ function saveWord() {
   // chrome.storage.sync.clear()
     var word = vocabWord.toLowerCase(),
         wordInfo = {
-            'def': wordDef,
-            'type': wordType
+          'type': wordType,
+          'def': wordDef
         };
     var jsonWordObj = {};
     jsonWordObj[word] = wordInfo;
     chrome.storage.sync.set(jsonWordObj, function () {
-        alert(`${word} has been added to your Vocab-U-List, ${wordInfo}`);
+        // alert(`${word} has been added to your Vocab-U-List, ${wordInfo}`);
+        alert(`${word} :  ${wordInfo}`);
+
     });
-    getRandomWord(jsonWordObj);
+    // getRandomWord(jsonWordObj);
     // chrome.storage.sync.get(null, function (data) { console.info(data) });
     // debugger
-    // chrome.storage.sync.get(null, function(items) {
-    //     var allKeys = Object.keys(items);
-    //     console.log(jsonWordObj);
-    //     console.log(jsonWordObj[allKeys[0]]);
-    //     console.log(jsonWordObj[allKeys[0].type]);
-    //     console.log(jsonWordObj[allKeys[0].def]);
-    // });
+    chrome.storage.sync.get(null, function(jsonWordObj) {
+        var allKeys = Object.keys(jsonWordObj);
+        console.log(jsonWordObj);
+        console.log(jsonWordObj[allKeys[0]]);
+        console.log(jsonWordObj[allKeys[0]].type);
+        console.log(jsonWordObj[allKeys[0]].def);
+    });
 }
+
 
 function getRandomWord(jsonWordObj){
   chrome.storage.sync.get(null, function(jsonWordObj) {
@@ -79,7 +82,7 @@ function getRandomWord(jsonWordObj){
 
 
 //called when word successfully added to list
-function getDefinition(){
+function dictionaryAjax(){
   let dictionaryURL = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + vocabWord + "?key=d3f5c888-db47-4c06-8c91-c65cc8a39137";
 
   //initialize ajax call to MW dictionary to get meaning of the word.
@@ -96,47 +99,52 @@ function getDefinition(){
         //set word type
         //responseXML gets data as XML data
         let word = xmlhttpRequest.responseXML.firstChild.getElementsByTagName("entry")[0];
-        debugger
-        wordType = word.getElementsByTagName("fl")[0].firstChild.nodeValue;
-
-        //set definition
-        wordDef = [];
-        for(let i = 0; i < word.getElementsByTagName("dt").length; i ++) {
-          if (!word.getElementsByTagName("dt")[i].firstChild.nodeValue){
-            for(let j = 0; j < word.getElementsByTagName("dt")[i].getElementsByTagName("un").length; j++) {
-              if (word.getElementsByTagName("dt")[i].getElementsByTagName("un")[j].firstChild.nodeValue){
-                let formatedWordUn = word.getElementsByTagName("dt")[i].getElementsByTagName("un")[j].firstChild.nodeValue.replace(/:/gi, '') || word.getElementsByTagName("dt")[i].getElementsByTagName("un")[j].firstChild.nodeValue;
-                word.getElementsByTagName("dt")[i];
-                if (formatedWordUn !== "") {
-                  wordDef.push(formatedWordUn);
-                }
-              }
-            }
-          } else {
-            // debugger
-            let formatedWord = word.getElementsByTagName("dt")[i].firstChild.nodeValue.replace(/:/gi, '') ||    word.getElementsByTagName("dt")[i].firstChild.nodeValue;
-            if (formatedWord !== "") {
-              wordDef.push(formatedWord);
-            }
-          }
-        }
-        // console.log(word);
-        // console.log(wordType);
-        // console.log(wordDef);
-        // debugger
-        alert(`vocabWord = ${vocabWord}; word = ${word}; type = ${wordType};  def = ${wordDef}`);
-        if (word) {
-          saveWord();
-        } else {
-          alert ("**ERROR** Word cannot be found in dictionary. Unable to be added to Vocab-U-List.");
-        }
-
+        getWordInfo(word);
       } else {
         alert("**ERROR** Word cannot be found in dictionary. Unable to be added to Vocab-U-List.");
       }
     }
   };
+}
 
+function getWordInfo(word) {
+  if (!word.getElementsByTagName("fl")[0]) {   //if no word type returned, word not in dictionary
+    alert (`We're sorry. Unable to find ${vocabWord} in dictionary -- not added to Vocab-U-List.`);
+  } else {
+    wordType = word.getElementsByTagName("fl")[0].firstChild.nodeValue;
+    wordDef = [];
+
+    for(let i = 0; i < word.getElementsByTagName("dt").length; i ++) {
+      if (!word.getElementsByTagName("dt")[i].firstChild.nodeValue){
+        for(let j = 0; j < word.getElementsByTagName("dt")[i].getElementsByTagName("un").length; j++) {
+          if (word.getElementsByTagName("dt")[i].getElementsByTagName("un")[j].firstChild.nodeValue){
+            let wordUn = word.getElementsByTagName("dt")[i].getElementsByTagName("un")[j].firstChild.nodeValue;
+            let formatedDef = wordUn.replace(/:/gi, '') || wordUn;
+            if (formatedDef !== "" && formatedDef !== ":" && wordDef.length < 4) {
+              wordDef.push(formatedDef);
+            }
+          }
+        }
+      } else {
+        // debugger
+        let wordDt = word.getElementsByTagName("dt")[i].firstChild.nodeValue;
+        let formatedDef = wordDt.replace(/:/gi, '') || wordDt;
+        if (formatedDef !== "" && formatedDef !== ":" && wordDef.length < 4) {
+          wordDef.push(formatedDef);
+        }
+      }
+    }
+    // console.log(word);
+    // console.log(wordType);
+    // console.log(wordDef);
+    // debugger
+    alert(`vocabWord = ${vocabWord}; word = ${word}; type = ${wordType};  def = ${wordDef}`);
+    // if (word) {
+      saveWord();
+    // } else {
+    //   alert ("**ERROR** Word cannot be found in dictionary. Unable to be added to Vocab-U-List.");
+    // }
+  }
 }
 
 //MY DICTIONARY INFO:
